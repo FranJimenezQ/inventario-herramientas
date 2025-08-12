f390cf5bace1abea5d2589933881923724f23e7a
Fran Jimenes <flakundo@gmail.com>
2025-08-12 17:06:18 -0600

    test 2

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async()=>{
         //const uri = 'mongodb+srv://flakundo%40gmail.com:Cofla051984@cluster0.yg1nm.mongodb.net/inventario_db?retryWrites=true&w=majority&appName=Cluster0';
         //mongodb+srv://franjimenez:F123j@cluster0.yg1nm.mongodb.net/
         await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        //revisar conexion a Db
+        //revisar conexion a Db en mongo
         console.log('conectado a la base de datos');
     }
     catch (e) {
