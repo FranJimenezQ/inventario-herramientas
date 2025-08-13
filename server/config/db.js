4b0be024b557bd8a9c07ab48651965ac6bf0974b
Fran Jimenes <flakundo@gmail.com>
2025-08-13 16:39:56 -0600

    se agrego archivo ignore en server, y se modifico la conexion con DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
+
 const connectDB = async()=>{
     try {
-         const uri = 'mongodb+srv://franjimenez:F123j@cluster0.yg1nm.mongodb.net/inventario_db?retryWrites=true&w=majority&appName=Cluster0';
-        //const uri = 'mongodb+srv://flakundo%40gmail.com:Cofla051984@cluster0.yg1nm.mongodb.net/inventario_db?retryWrites=true&w=majority&appName=Cluster0';
-        //mongodb+srv://franjimenez:F123j@cluster0.yg1nm.mongodb.net/
-        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        //revisar conexion a Db en mongo
+        
+        const uri = process.env.MONGODB_URI;
+
+        await mongoose.connect(uri);
         console.log('conectado a la base de datos');
     }
     catch (e) {
