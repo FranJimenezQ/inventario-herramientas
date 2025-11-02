69e88f0ca98608109433f2488e03c3f4f09daa10
Fran Jimenes <flakundo@gmail.com>
2025-11-01 18:51:12 -0600

    fix de error en ruta de variable BE

--- client-app/src/environments/environments.prod.ts
+++ client-app/src/environments/environments.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://inventario-herramientas-uu68.onrender.com/'
+    apiUrl: 'https://inventario-herramientas-uu68.onrender.com'
 };
